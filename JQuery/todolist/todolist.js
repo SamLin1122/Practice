d35b431a3051935791e375850349e781210a0f01
SamLin1122 <zhlin1122@gmail.com>
2019-08-04 10:50:01 +0800

    add candy

--- JQuery/todolist/todolist.js
+++ JQuery/todolist/todolist.js
@@ -2,17 +2,13 @@ $("ul").on("click","li",function () {
   $(this).toggleClass("completed");
 });
 $("ul").on("click","span", function (e) {
-  console.log(this)
   $(this).parent().slideUp(500,function(){
     this.remove();
   })
-  // e.stopPropagation();
 })
 $("input[type='text']").keypress(function(e){
   if(e.which===13){
-  $("ul").append(`<li><span><i></i></span>${this.value}</li>`);
-  $("span i").addClass("far");
-  $("span i").addClass("fa-trash-alt");
+  $("ul").append(`<li><span><i class="far fa-trash-alt"></i></span>${this.value}</li>`);
   $(this).val("");
 };
 })
